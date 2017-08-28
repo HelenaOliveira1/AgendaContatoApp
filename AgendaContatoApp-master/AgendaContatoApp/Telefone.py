0fc3a25d922f0ce38e14175ef4e7ee2c18a84208
ulyssesMonte <ulyssesmontealverne@hotmail.com>
2017-08-28 07:37:09 -0300

    Delete Telefone.py

--- AgendaContatoApp-master/AgendaContatoApp/Telefone.py
+++
@@ -1,8 +0,0 @@
-listaTelefone = []
-
-class Telefone():
-    def __init__(self, numero, ddd, codigoPais, listaTelefone):
-        self.numero = numero
-        self.ddd = ddd
-        self.codigoPais = codigoPais
-        listaTelefone = listaTelefone.append("(%i%i) %s"%(self.codigoPais, self.ddd, self.numero))
