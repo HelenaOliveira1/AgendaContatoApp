6bbbfc5a47047c42ed911c6cac1686e595236b81
ulyssesMonte <ulyssesmontealverne@hotmail.com>
2017-08-28 07:36:56 -0300

    Delete Pessoa.py

--- AgendaContatoApp-master/AgendaContatoApp/Pessoa.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-class Pessoa():
-    def __init__(self, nome, email, ano, mes, dia):
-        self.nome = nome
-        self.nascimento = datetime.date(ano, mes, dia)
-        self.email = email
