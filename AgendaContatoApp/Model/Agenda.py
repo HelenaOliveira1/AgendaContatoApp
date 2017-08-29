f2257e72a904c63532ceda27b8dc83ff2ecd5fe3
ulyssesMonte <ulyssesmontealverne@hotmail.com>
2017-08-29 17:34:30 -0300

    Update Agenda.py

--- AgendaContatoApp/Model/Agenda.py
+++ AgendaContatoApp/Model/Agenda.py
@@ -6,14 +6,19 @@ class Agenda(Contato):
     def __init__(self, nome, email, ano, mes, dia):
         self.proprietario = Pessoa(nome, email, ano, mes, dia)
 
-    def ContarContatos(self):
+    def ContarContatos(self, contagem):
         print("Quantidade de Contatos: ")
+        return contagem
 
-    def ListarContatos(self):
+    def ListarContatos(self, listaContato):
         print("Sua lista de Contatos: ")
+        for contato in listaContato:
+            print(contato)
 
     def IncluirContatos(self):
         print("Incluir novo contato: ")
+        Contato.AddContato(self, self.pessoa, self.criacao, self.telefone, contagem, listaContato)
+        
 
     def ExcluirContato(self):
         print("Excluir Contato: ")
