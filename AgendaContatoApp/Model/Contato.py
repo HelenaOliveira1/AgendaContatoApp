b3b150a9ae99ec6c3dde5e0900e080e85f61fa6a
ulyssesMonte <ulyssesmontealverne@hotmail.com>
2017-08-29 17:20:58 -0300

    Update Contato.py

--- AgendaContatoApp/Model/Contato.py
+++ AgendaContatoApp/Model/Contato.py
@@ -3,6 +3,7 @@ from AgendaContatoApp.Model.Pessoa import Pessoa
 from AgendaContatoApp.Model.Telefone import Telefone
 
 listaContato = []
+contagem = 0
 
 class Contato(Telefone):
     def __init__(self, nome, email, ano, mes, dia, numero, ddd, codigoPais):
@@ -10,6 +11,7 @@ class Contato(Telefone):
         self.pessoa = Pessoa(nome, email, ano, mes, dia)
         super(Contato, self).__init__(numero, ddd, codigoPais)
         listaContato = listaContato.append("Proprietário:", self.pessoa, "Criação:", self.criacao, "Telefone: ", self.telefone)
+        contagem += 1
 
     def listarTelefones(self, listaTelefone):
         print("Seus Telefones salvos: ")
