6f65671e0b3e5e1e215f6c911afdd0888255d04a
ulyssesMonte <ulyssesmontealverne@hotmail.com>
2017-08-30 08:41:21 -0300

    Add Try e Except

--- AgendaContatoApp/Model/Contato.py
+++ AgendaContatoApp/Model/Contato.py
@@ -1,28 +1,34 @@
-import datetime
-from AgendaContatoApp.Model.Pessoa import Pessoa
-from AgendaContatoApp.Model.Telefone import Telefone
+while True:
+    try:
 
-listaContato = []
-contagem = 0
-numLinhas= 0
+        import datetime
+        from AgendaContatoApp.Model.Pessoa import Pessoa
+        from AgendaContatoApp.Model.Telefone import Telefone
 
-class Contato(Telefone):
-    def __init__(self, nome, email, ano, mes, dia, numero, ddd, codigoPais):
-        self.criacao = datetime.datetime.now()
-        self.pessoa = Pessoa(nome, email, ano, mes, dia)
-        super(Contato, self).__init__(numero, ddd, codigoPais)
+        listaContato = []
+        contagem = 0
+        numLinhas= 0
+
+        class Contato(Telefone):
+            def __init__(self, nome, email, ano, mes, dia, numero, ddd, codigoPais):
+                self.criacao = datetime.datetime.now()
+                self.pessoa = Pessoa(nome, email, ano, mes, dia)
+                super(Contato, self).__init__(numero, ddd, codigoPais)
     
-    def AddContatos(self, self.pessoa, self.criacao, self.telefone, contagem, listaContato):
-        nome = input("Digite o nome do contato: ")
-        email = input("Digite Email do contato: ")
-        dia = input("Digite dia do nascimento: ")
-        mes = input("Digite mês do nascimento: ")
-        ano = input("Digite ano do nascimento: ")
-        contato = Contato(nome,email,ano,mes,dia,listaTelefone)
-        listaContato.append(contato)
+            def AddContatos(self, self.pessoa, self.criacao, self.telefone, contagem, listaContato):
+                nome = input("Digite o nome do contato: ")
+                email = input("Digite Email do contato: ")
+                dia = input("Digite dia do nascimento: ")
+                mes = input("Digite mês do nascimento: ")
+                ano = input("Digite ano do nascimento: ")
+                contato = Contato(nome,email,ano,mes,dia,listaTelefone)
+                listaContato.append(contato)
     
 
-    def listarTelefones(self, listaTelefone):
-        print("Seus Telefones salvos: ")
-        for telefone in listaTelefone:
-            print("Telefone: ", telefone)
+            def listarTelefones(self, listaTelefone):
+                print("Seus Telefones salvos: ")
+                for telefone in listaTelefone:
+                    print("Telefone: ", telefone)
+                    
+    Except:
+        print("Ocorreu um Erro!")
